53317185ba9a6f66fb00a5aecf5fc32904a5cff5
Jair Torezone <jairtorezone@gmail.com>
2024-09-28 19:04:36 -0300

    created login.html

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import  Flask, render_template, url_for
+from forms import FormLogin, FormCriarConta
 
 app = Flask(__name__)
 
 lista_usuarios = ['Torezone', 'Oliveira', 'Lourenço', 'Ribeiro', 'Padula']
+app.config['SECRET_KEY'] = '5a876d64f21172cd25d0c3c4fc4ce67e'
 
 @app.route("/")
 def home():
@@ -18,7 +20,9 @@ def usuarios():
 
 @app.route("/login")
 def login():
-    return render_template('login.html')
+    form_login = FormLogin()
+    form_criarconta = FormCriarConta()
+    return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
 
 
 if __name__ == '__main__':
